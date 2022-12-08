0afb1b2520e0bcfcbe3af0aab50a1b226a2bd8c6
louisgiron <louis.giron@engie.com>
2022-12-08 15:31:24 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Terraform templates
+
+## Amazon S3 bucket with NextJS
+> amazon-s3-bucket-nextjs-template
+
+```Create Amazon S3 with specific configurations to use the S3 bucket with NextJS```
---
+++ amazon-s3-bucket-nextjs-template/main.tf
@@ -0,0 +1,82 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-3"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "girondom-latabledesgironffroy-dev"
+
+  tags = {
+    CostCenter  = "La Table Des Gironffroy"
+    MyProject   = "La Table Des Gironffroy"
+    ProjectType = "PersonalProject"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_cors_configuration" "bucket_cors_configuration" {
+  bucket = aws_s3_bucket.b.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST", "GET"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "bucket_ownership_controls" {
+  bucket = aws_s3_bucket.b.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block_all_public_access_to_bucket" {
+  bucket = aws_s3_bucket.b.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "allow_iam_user_to_interact_with_bucket" {
+  bucket = aws_s3_bucket.b.id
+  policy = data.aws_iam_policy_document.allow_iam_user_to_interact_with_bucket.json
+}
+
+data "aws_iam_policy_document" "allow_iam_user_to_interact_with_bucket" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::272517345723:user/girondom_latabledesgironffroy_user"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+    ]
+
+    resources = [
+      aws_s3_bucket.b.arn,
+      "${aws_s3_bucket.b.arn}/*",
+    ]
+  }
+}
